17b6073fb09bbdc6cdfb8eb34259026cc645ee2d
Elat <talebjorn@gmail.com>
2022-03-23 19:58:36 +0100

    Add main functions, add regex, add game function

--- script.js
+++ script.js
@@ -1,10 +1,101 @@
 
-//It should have a function that picks 'rock' 'paper' 'scissors' at random and returns the value
-function computerChoice(computerSelection) {
+const rockPaperScissor = ['rock', 'paper', 'scissors'];
 
-    let rockPaperScissor = ['rock', 'paper', 'scissors'];
+
+function computerPlay(computerSelection) {
+
     computerSelection = rockPaperScissor[Math.floor(Math.random()* rockPaperScissor.length)];
-    console.log(computerSelection);
+    return computerSelection;
+    
 }
 
-computerChoice();
+
+const defeat = 'Defeat';
+const victory = 'Victory';
+
+let userScore = 0;
+let computerScore = 0;
+
+function playRound (userSelection, computerSelection) {
+
+     let regex = /^rock$|^paper$|^scissors$/i;
+     let rockEx = /rock/i;
+     let paperEx = /paper/i;
+     let ScissorEx = /scissors/i;
+
+     userSelection = prompt('Rock, paper, scissors:');
+     computerSelection = computerPlay();
+     
+     if (userSelection === null || userSelection === '') {
+         alert('You can\'t beat the computer with that attitude. Input rock, paper or scissors!') 
+     }  if(userSelection.match(regex)) {
+            if (computerSelection === 'rock' && userSelection.match(paperEx)) {
+             
+                console.log('Computer picked:', computerSelection, 'You picked:', userSelection);
+                userScore++;
+                return victory;
+
+            } else if (computerSelection === 'scissors' && userSelection.match(paperEx)) {
+                console.log('Computer picked:', computerSelection, 'You picked:', userSelection);
+                computerScore++;
+                return defeat;
+                
+            } else if (computerSelection === 'paper' && userSelection.match(rockEx)) {
+             
+                console.log('Computer picked:', computerSelection, 'You picked:', userSelection);
+                computerScore++;
+                return defeat;
+
+            } else if (computerSelection === 'scissors' && userSelection.match(rockEx)) {
+             
+                console.log('Computer picked:', computerSelection, 'You picked:', userSelection);
+                userScore++;
+                return victory;
+
+            } else if (computerSelection === 'rock' && userSelection.match(ScissorEx)) {
+              
+                console.log('Computer picked:', computerSelection, 'You picked:', userSelection);
+                computerScore++;
+                return defeat;
+
+            } else if (computerSelection === 'paper' && userSelection.match(ScissorEx)) {
+                
+                console.log('Computer picked:', computerSelection, 'You picked:', userSelection);
+                userScore++;
+                return victory;
+
+            } else if (userSelection.toLowerCase() === computerSelection) {
+                console.log('It\'s a draw. ', 'Computer picked:', computerSelection, 'You picked:', userSelection);
+            
+            }
+      } else {
+        console.log('Invalid input');
+        alert('Input rock, paper or scissors!');
+      }
+    
+}
+
+        function game() {
+            
+            for(let i = 0; i <= 5; i++) {
+                if (i <= 5){ 
+                    console.log(computerScore, userScore);
+                    playRound()
+                     if (i === 5 && userScore > computerScore) {
+                         console.log('You win', computerScore, userScore)
+                        return 'You have won';
+
+                    } else if (i === 5 && computerScore > userScore) {
+                         console.log('Computer wins', computerScore, userScore)
+                         return 'You have lost';
+                   } else if (i === 5 && computerScore === userScore) {
+                       console.log('There will be no winners today', computerScore, userScore);
+                   }
+                
+            }
+            
+          }
+        }
+
+        game();
+        
