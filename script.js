1ff33fc2f42a46ed1e2398056562c1a4e64d816a
Elat <talebjorn@gmail.com>
2022-05-05 13:47:48 +0200

    Simplify game logic, rewrite initial code

--- script.js
+++ script.js
@@ -1,124 +1,66 @@
+const div = document.getElementById('div');
 
-//UI 
-const div = document.getElementById("div");
+    const btn = document.getElementsByClassName('btn');
+        Array.from(btn).forEach(item => {
+            item.addEventListener('click', (e) => {
+                let userSelection = e.target.textContent;
+                playRound(userSelection, computerPlay());
+                console.log('You ' + userSelection);
+                return userSelection; 
+            });
+        });
 
-function userSelection() {
+const display = document.getElementById('display');
 
+    const score = document.createElement('p');
+    score.textContent = 'Current Score: ' + 'User: 0 ' + ' Computer: 0';
 
-}
-const rock = document.createElement('button'); 
-rock.textContent = 'ROCK';
-rock.addEventListener('click', (e) => {
-    console.log(e.target.textContent);
-    return e.target;
-});
+    const lastChoice = document.createElement('p');
+    lastChoice.textContent = 'You picked: - - - - Computer picked: - - - -';
 
-const paper = document.createElement('button');
-paper.textContent = 'PAPER';
-paper.addEventListener('click', (e) => {
-    console.log(e.target.textContent);
-    return e.target;
-});
-const scissors = document.createElement('button');
-scissors.textContent = 'SCISSORS';
-scissors.addEventListener('click', (e) => {
-    console.log(e.target.textContent);
-    return e.target;
-});
+    const winner = document.createElement('p');
+    winner.textContent = 'Whomst\'vbeen winning?!';
 
-
-
-div.appendChild(rock);
-div.appendChild(paper);
-div.appendChild(scissors);
-
-
-const rockPaperScissor = ['rock', 'paper', 'scissors']; //array for computerSelection
-
-function computerPlay(computerSelection) {  
-    //Picks a random index of the array and returns the string.
-                                        
-    computerSelection = rockPaperScissor[Math.floor(Math.random()* rockPaperScissor.length)];
-    console.log(computerSelection)
+function computerPlay() {
+    const rockPaperScissors  = ['ROCK', 'PAPER', 'SCISSORS', 'LIZARD', 'SPOCK'];
+    const computerSelection = rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)];
+    console.log('Computer ' + computerSelection);
     return computerSelection;
-    
 }
-
-const defeat = 'Defeat';
-const victory = 'Victory';
 
 let userScore = 0;
 let computerScore = 0;
 
-function playRound (userSelection, computerSelection) {
+function playRound(user, computer) {
+  
+    console.log('user', user, 'computer', computer)
+    switch(true) {
+        case user === 'ROCK': 
+        computer === 'PAPER' || 'SPOCK' ? computerScore++ : userScore++;
+        break;
+        case user === 'PAPER': 
+        computer === 'SPOCK' || 'LIZARD' ? computerScore++ : userScore++;
+        break;
+        case user === 'SCISSORS': 
+        computer === 'ROCK' || 'ROCK' ? computerScore++ : userScore++;
+        break;
+        case user === 'LIZARD': 
+        computer === 'SCISSORS' || 'ROCK' ? computerScore++ : userScore++;
+        break;
+        case user === 'SPOCK':
+        computer === 'LIZARD' || 'PAPER' ? computerScore++ : userScore++;
+        break;
+        default: 
+        'DRAW';
+    }
+    score.textContent = 'Current Score: ' + ' User: ' + userScore + ' Computer: ' + computerScore;
+    lastChoice.textContent = 'You picked: ' + user + ' Computer picked: ' + computer;
+    console.log(userScore, computerScore)
+    return userScore, computerScore;
+    
+};
 
-     userSelection = e.target;
-     computerSelection = computerPlay();
 
-            if (computerSelection === 'rock' && userSelection.match(paperEx)) {
-             
-                console.log('Computer picked:', computerSelection, 'You picked:', userSelection);
-                userScore++;
-                return victory;
-
-            } else if (computerSelection === 'scissors' && userSelection.match(paperEx)) {
-                console.log('Computer picked:', computerSelection, 'You picked:', userSelection);
-                computerScore++;
-                return defeat;
-                
-            } else if (computerSelection === 'paper' && userSelection.match(rockEx)) {
-             
-                console.log('Computer picked:', computerSelection, 'You picked:', userSelection);
-                computerScore++;
-                return defeat;
-
-            } else if (computerSelection === 'scissors' && userSelection.match(rockEx)) {
-             
-                console.log('Computer picked:', computerSelection, 'You picked:', userSelection);
-                userScore++;
-                return victory;
-
-            } else if (computerSelection === 'rock' && userSelection.match(ScissorEx)) {
-              
-                console.log('Computer picked:', computerSelection, 'You picked:', userSelection);
-                computerScore++;
-                return defeat;
-
-            } else if (computerSelection === 'paper' && userSelection.match(ScissorEx)) {
-                
-                console.log('Computer picked:', computerSelection, 'You picked:', userSelection);
-                userScore++;
-                return victory;
-
-            } else if (userSelection.toLowerCase() === computerSelection) {
-                console.log('It\'s a draw. ', 'Computer picked:', computerSelection, 'You picked:', userSelection);
-            
-            }
-        }
-
-        // function game() {
-        //     //Calls playRound() until one of the players reaches 5 pts.
-        //     for(let i = 0; i <= 10; i++) {
-        //         if (i <= 10){ 
-        //             console.log(computerScore, userScore);
-        //             playRound()
-        //              if (userScore === 5) {
-        //                  console.log('You win', computerScore, userScore)
-        //                 return 'You have won';
-
-        //             } else if (computerScore === 5) {
-        //                  console.log('Computer wins', computerScore, userScore)
-        //                  return 'You have lost';
-        //            } else if (i === 10 && computerScore === userScore) {
-        //                console.log('There will be no winners today', computerScore, userScore);
-        //            }
-                
-        //     }
-            
-        //   }
-        // }
-
-        // game();
-        
-
-        
+    display.appendChild(score);
+    display.appendChild(lastChoice);
+    display.appendChild(winner);
